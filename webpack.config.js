28ea7cba2ac6a11761d8907ef20e3da8ab6c9507
Ryan Buchholtz <ryanscottbuchholtz@gmail.com>
2016-06-08 14:09:52 -0700

    uglify complete

--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,9 @@ var packageData = require('./package.json');
 var filename = [packageData.name, packageData.version, 'js'];
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var minify = process.argv.indexOf('--minify') != -1;
 
-module.exports = {
-    entry: {
-      main: [
-        path.resolve(__dirname, packageData.main),
-        'animate-css-webpack!./node_modules/animate-css-webpack/animate-css.config.js',
-      ]
-    },
-
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: filename.join('.'),
-    },
-    devtool: 'source-map',
-    plugins: [new HtmlWebpackPlugin({
+var plugins = [new HtmlWebpackPlugin({
       inject: 'head',
       template: 'index.html',
       minify: {
@@ -37,27 +25,55 @@ module.exports = {
     new ExtractTextPlugin('style.css'),
     new CopyWebpackPlugin([
             {from: 'assets', to: 'assets'}
-        ]),
-  ],
-  module: {
-    loaders: [
-      {
+        ])
+  ];
+
+if (minify) {
+    filename.splice(filename.length - 1, 0, 'min');
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+};
+
+module.exports = {
+    entry: {
+      main: [
+        path.resolve(__dirname, packageData.main),
+        'animate-css-webpack!./node_modules/animate-css-webpack/animate-css.config.js',
+      ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: filename.join('.'),
+    },
+    devtool: 'source-map',
+    plugins: plugins,
+    module: {
+      loaders: [
+        {
           test: /\.less$/,
           loader: ExtractTextPlugin.extract(
               // activate source maps via loader query
             'css?sourceMap!' +
             'less?sourceMap'
           )
-      },
-      { 
-          test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-      },
-      {   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"
-      },
-      {   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
-      }
-    ]
-  }
+          },
+        {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            loader: 'babel',
+            query: {
+              presets: ['es2015']
+            }
+          },
+        { 
+            test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+          },
+        {   test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff"
+          },
+        {   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"
+          }
+      ]
+    }
 };
 
 // ghpages.publish(path.join(__dirname, 'build'), callback);
